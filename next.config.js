532f1ec6f9df4497bee27690c4cf5134d1ad1dd8
russuxdesign <russuxdesign@gmail.com>
2026-08-05 23:18:54 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,8 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [
-      'covers.openlibrary.org',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'covers.openlibrary.org',
+      },
     ],
   },
 };
