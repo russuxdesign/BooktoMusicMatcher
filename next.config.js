2ac9aabdd60dac9342158d37b93468f892ac1add
russuxdesign <russuxdesign@gmail.com>
2026-08-05 23:34:08 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'covers.openlibrary.org',
-      },
-    ],
-  },
 };
 
 module.exports = nextConfig;
